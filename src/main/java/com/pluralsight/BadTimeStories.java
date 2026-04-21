a17f7550be3784537d9f9969651c111cccf03160
polinabreha <polinabreha@gmail.com>
2026-04-21 11:26:23 -0400

    change the numbers for the finishing program

--- src/main/java/com/pluralsight/BadTimeStories.java
+++ src/main/java/com/pluralsight/BadTimeStories.java
@@ -30,7 +30,7 @@ public class BadTimeStories {
                       
                       3. Mary had a little lamb\
                       
-                      4. Finish the program ( enter 0 )\
+                      4. Finish the program ( enter 4 )\
                       
                        Enter the number(1-3) :\s""");
               int storyNumber = scanner.nextInt();
@@ -64,7 +64,7 @@ public class BadTimeStories {
                       System.out.println(input);
                   }
                   bufferedReader.close();
-              } else if (storyNumber == 0) {
+              } else if (storyNumber == 4) {
                   System.out.println("----Bye see you again----");
                   run = false;
               }
@@ -72,7 +72,7 @@ public class BadTimeStories {
 
           }
       }catch (InputMismatchException e){
-          System.out.println("Invalid input! Please enter a number between 0-3.");
+          System.out.println("Invalid input! Please enter a number between 1-4.");
       }catch (IOException e) {
           System.out.println("I/O Error! Please try again.");
       }
